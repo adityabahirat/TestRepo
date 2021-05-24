d0da168d9fb942e84732cca5d48e7e7d839c9e09
adityabahirat <adityabahirat12@gmail.com>
2021-05-24 18:20:43 +0530

    testRepo is edited

--- src/Loc.java
+++ src/Loc.java
@@ -2,17 +2,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
+
 public class Loc {
 
 	public static void main(String[] args) {
+		
+		WebDriver driver;
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","D:\\Software\\Eclipse\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver= new ChromeDriver();
+		 driver= new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://facebook.com");
 		driver.findElement(By.id("email")).sendKeys("adityabahirat12@gmail.com");
 		driver.findElement(By.name("pass")).sendKeys("12345");
 		driver.findElement(By.linkText("Forgotten password?")).click();
+		
+	
 		
 
 	}
