e70d62a6200ddd3d78be6004443203a7ee19ab13
adityabahirat <adityabahirat12@gmail.com>
2021-05-25 22:40:49 +0530

    files added

--- src/branchDemoPackage/Test.java
+++ src/branchDemoPackage/Test.java
@@ -1,5 +1,10 @@
 package branchDemoPackage;
 
 public interface Test {
+	
+	 default void getData() {
 
+	System.out.println("aditya");
+	
+}
 }
--- src/branchDemoPackage/Testcase.java
+++ src/branchDemoPackage/Testcase.java
@@ -1,9 +1,11 @@
 package branchDemoPackage;
 
-public class Testcase {
+public class Testcase implements Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Aditya Bahirat");
 
 	}
 
